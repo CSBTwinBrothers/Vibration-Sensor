52d761a7e8994261f8df2ec46ea434393955ccda
CSBTwinBrothers <73055804+CSBTwinBrothers@users.noreply.github.com>
2020-10-30 14:29:32 +0530

    Create vibration_Sensor.ino

---
+++ vibration_Sensor.ino
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <stdio.h> 
+#define ON 1
+#define OFF 0
+int vibration_Sensor = A5;
+int LED = 13;
+int present_condition = 0;
+int previous_condition = 0;
+void setup() {
+pinMode(vibration_Sensor, INPUT);
+pinMode(LED, OUTPUT);
+}
+void led_blink(void);
+void loop() {
+previous_condition = present_condition;
+present_condition = digitalRead(A5); 
+if (previous_condition != present_condition) {
+led_blink(); 
+} else {
+digitalWrite(LED, OFF);
+}
+}
+void led_blink(void) {
+digitalWrite(LED, ON);
+delay(250);
+digitalWrite(LED, OFF);
+delay(250);
+digitalWrite(LED, ON);
+delay(250);
+digitalWrite(LED, OFF);
+delay(250);
+}
